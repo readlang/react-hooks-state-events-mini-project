9d339d14b9a489c5fe7730349f2f8da65fdb486e
Read Langworthy <rlangworthy@gmail.com>
2021-12-02 21:14:15 -0800

    in process 2

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+console.log({ CATEGORIES /* , TASKS*/ });
 
 
 
 function App() {
   const [catFilter, setCatFilter ] = useState( "All" )
 
-  setCatFilter("Code")
+  //setCatFilter("Code")
 
   return (
     <div className="App">
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -2,21 +2,32 @@ import React, {useState} from "react";
 
 function CategoryFilter({categories, catFilter, setCategoryFilter} ) {
   function handleFilterClick(event) {
-    console.log(event.target.textContent)
-    console.log(event.target)
+    console.log("1", event.target.textContent)
+    console.log("2", event.target)
     setCategoryFilter(event.target.textContent)
     
     //event.target.className = "selected"
 
+    /// Stuck here with an error
+  }
+  console.log("3", catFilter)
+
+  function mapper(x) {
+    let classVari = ""
+    if (x === catFilter) {
+      classVari = "selected"
+      console.log("selected")
+    } 
+
+    console.log("4", x)
+
+    return( <button key={x} onClick={handleFilterClick} className={classVari} > {x} </button> )
   }
 
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */
-        categories.map(x=>( <button key={x} onClick={() => handleFilterClick} 
-          className={ catFilter === x ? "selected" : "" }  >{x}</button> ))
-      }
+      {categories.map(mapper)}
     </div>
   );
 }
